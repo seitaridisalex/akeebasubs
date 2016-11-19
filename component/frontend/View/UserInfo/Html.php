35de5de66efb6a7848328aac63eeeb3113867132
Todor Iliev <todor@itprism.com>
2016-11-19 18:20:26 +0200

    Added method getFieldValue() to UserInfo\Html

--- component/frontend/View/UserInfo/Html.php
+++ component/frontend/View/UserInfo/Html.php
@@ -13,5 +13,39 @@ defined('_JEXEC') or die;
 
 class Html extends View
 {
+    /**
+     * Get the value of a field from the session cache. If it's empty use the value from the user parameters cache.
+     *
+     * @param   string  $fieldName    The name of the field
+     * @param   array   $emptyValues  A list of values considered to be "empty" for the purposes of this method
+     *
+     * @return  mixed  The field value
+     */
+    public function getFieldValue($fieldName, array $emptyValues = [])
+    {
+        $cacheValue = null;
+        $userparamsValue = null;
 
+        if (isset($this->cache[$fieldName]))
+        {
+            $cacheValue = $this->cache[$fieldName];
+        }
+
+        if (isset($this->userparams->{$fieldName}))
+        {
+            $userparamsValue = $this->userparams->{$fieldName};
+        }
+
+        if (is_null($cacheValue))
+        {
+            return $userparamsValue;
+        }
+
+        if (!empty($emptyValues) && in_array($cacheValue, $emptyValues))
+        {
+            return $userparamsValue;
+        }
+
+        return $cacheValue;
+    }
 }
