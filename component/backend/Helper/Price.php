c4e2926e97bcabf48507f7a59f45ec92bd533b81
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-04-11 20:38:26 +0300

    Refactor the asprice content plugin
    Extract the pricing information helper functions from the Levels view
    into its own helper class.

---
+++ component/backend/Helper/Price.php
@@ -0,0 +1,310 @@
+<?php
+/**
+ * @package     Akeeba\Subscriptions\Admin\Helper
+ * @subpackage
+ *
+ * @copyright   A copyright
+ * @license     A "Slug" license name e.g. GPL2
+ */
+
+namespace Akeeba\Subscriptions\Admin\Helper;
+
+use Akeeba\Subscriptions\Site\Model\Levels;
+use Akeeba\Subscriptions\Site\Model\Subscriptions;
+use FOF30\Container\Container;
+
+abstract class Price
+{
+	/**
+	 * Cache of pricing information per subscription level, required to cut down on queries in the Strappy layout.
+	 *
+	 * @var  object[]
+	 */
+	protected static $pricingInformationCache = [];
+
+	/**
+	 * The subscription level IDs for which the currently logged in user has an active subscription.
+	 *
+	 * @var   array
+	 */
+	protected static $subIDs = null;
+
+	/**
+	 * The application container for Akeeba Subscriptions
+	 *
+	 * @var   Container
+	 */
+	protected static $container = null;
+
+	/**
+	 * Gets the container instance for Akeeba Subscriptions
+	 *
+	 * @return  Container
+	 */
+	protected static function getContainer()
+	{
+		if (is_null(self::$container))
+		{
+			self::$container = Container::getInstance('com_akeebasubs');
+		}
+
+		return self::$container;
+	}
+
+	/**
+	 * Parameters for calculating and displaying prices
+	 *
+	 * @var   \stdClass
+	 */
+	protected static $pricingParameters = null;
+
+	/**
+	 * Returns the subscription level IDs for which the currently logged in user has an active subscription. Used for
+	 * calculating sign-up fees.
+	 *
+	 * @return  int[]
+	 */
+	public static function getSubIDs()
+	{
+		if (is_null(self::$subIDs))
+		{
+			$container = self::getContainer();
+			$subIDs    = array();
+			$user      = \JFactory::getUser();
+
+			if ($user->id)
+			{
+				/** @var Subscriptions $mysubs */
+				$mysubs = $container->factory->model('Subscriptions')->tmpInstance();
+				$mysubs
+					->user_id($user->id)
+					->paystate('C')
+					->get(true);
+
+				if (!empty($mysubs))
+				{
+					foreach ($mysubs as $sub)
+					{
+						$subIDs[] = $sub->akeebasubs_level_id;
+					}
+				}
+
+				$subIDs = array_unique($subIDs);
+			}
+
+			self::$subIDs = $subIDs;
+		}
+
+		return self::$subIDs;
+	}
+
+	/**
+	 * Get the price calculation parameters
+	 *
+	 * @return  \stdClass
+	 */
+	public static function getPricingParameters()
+	{
+		if (is_null(self::$pricingParameters))
+		{
+			$container = self::getContainer();
+			/** @var \Akeeba\Subscriptions\Site\Model\TaxHelper $taxModel */
+			$taxModel  = self::getContainer()->factory->model('TaxHelper')->tmpInstance();
+			$user = \JFactory::getUser();
+			$showLocalPrices = $container->params->get('showlocalprices', 1);
+			$taxParams  = $taxModel->getTaxDefiningParameters();
+			$exchangeRate = 1.00;
+			$country = '';
+			$localCurrency = '';
+			$localSymbol = '';
+
+			if ($showLocalPrices)
+			{
+				$country = $taxParams['country'];
+				Forex::updateRates(false, $container);
+				$temp                = Forex::convertToLocal($taxParams['country'], 1.00, $container);
+				$exchangeRate  = $temp['rate'];
+				$localCurrency = $temp['currency'];
+				$localSymbol   = $temp['symbol'];
+
+				// Do not show foreign exchange conversions unless the exchange rate is different than unity
+				if (abs($exchangeRate - 1.00) <= 0.000001)
+				{
+					$showLocalPrices = false;
+				}
+			}
+
+			self::$pricingParameters = (object) [
+				'taxModel'         => $taxModel,
+				'showVat'          => $container->params->get('showvat', 0),
+				'taxParams'        => $taxParams,
+				'includeSignup'    => $container->params->get('includesignup', 2),
+				'includeDiscount'  => $user->guest ? false : $container->params->get('includediscount', 0),
+				'renderAsFree'     => $container->params->get('renderasfree', 0),
+				'showLocalPrices'  => $showLocalPrices,
+				'country'          => $country,
+				'exchangeRate'     => $exchangeRate,
+				'localCurrency'    => $localCurrency,
+				'localSymbol'      => $localSymbol,
+				'currencyPosition' => $container->params->get('currencypos', 'before'),
+			];
+		}
+
+		return self::$pricingParameters;
+	}
+
+	/**
+	 * Returns the pricing information for a subscription level. Used by the view templates to avoid code duplication.
+	 *
+	 * @param   \Akeeba\Subscriptions\Site\Model\Levels  $level  The subscription level
+	 *
+	 * @return  object
+	 */
+	public static function getLevelPriceInformation(Levels $level)
+	{
+		$levelKey = $level->getId() . '-' . $level->slug;
+
+		if (isset(self::$pricingInformationCache[$levelKey]))
+		{
+			return self::$pricingInformationCache[$levelKey];
+		}
+
+		$signupFee = 0;
+		$discount = 0;
+		$levelPrice = $level->price;
+		$vatMultiplier = 1.0;
+		$vatRule = (object)[
+			'match'   => 0,    // How many parameters matched exactly
+			'fuzzy'   => 0,    // How many parameters matched fuzzily
+			'taxrate' => 0, // Tax rate in percentage points (e.g. 12.3 means 12.3% tax)
+			'id'      => 0, // The ID of the tax rule in effect
+		];
+
+		$subIDs = self::getSubIDs();
+		$params = self::getPricingParameters();
+
+		if (!in_array($level->akeebasubs_level_id, $subIDs) && ($params->includeSignup != 0))
+		{
+			$signupFee = (float)$level->signupfee;
+		}
+
+		if ($params->showVat)
+		{
+			$vatRule = $params->taxModel->getTaxRule(
+				$level->akeebasubs_level_id, $params->taxParams['country'], $params->taxParams['state'],
+				$params->taxParams['city'], $params->taxParams['vies']
+			);
+
+			$vatMultiplier = (100 + (float)$vatRule->taxrate) / 100;
+		}
+
+		$preDiscount = max($levelPrice, 0.0);
+
+		if ($params->includeDiscount)
+		{
+			/** @var \Akeeba\Subscriptions\Site\Model\Subscribe $subscribeModel */
+			$subscribeModel = self::getContainer()->factory->model('Subscribe')->savestate(0);
+			$subscribeModel->setState('id', $level->akeebasubs_level_id);
+			$subscribeModel->setState('slug', $level->slug);
+			$subValidation = $subscribeModel->getValidation(true);
+			$discount = $subValidation->price->discount;
+			$levelPrice = $level->price - $discount;
+		}
+
+		if ($params->includeSignup == 1)
+		{
+			if (($levelPrice + $signupFee) < 0)
+			{
+				$levelPrice = -$signupFee;
+			}
+
+			$priceForFormatting = ($levelPrice + $signupFee) * $vatMultiplier;
+			$formattedPrice     = sprintf('%1.02F', $priceForFormatting);
+			$preDiscount        = max(($preDiscount + $signupFee) * $vatMultiplier, 0);
+			$levelPrice += $signupFee;
+		}
+		else
+		{
+			if ($levelPrice < 0)
+			{
+				$levelPrice = 0;
+			}
+
+			$priceForFormatting = ($levelPrice) * $vatMultiplier;
+			$formattedPrice = sprintf('%1.02F', $priceForFormatting);
+			$preDiscount = $preDiscount * $vatMultiplier;
+		}
+
+		$dotpos = strpos($formattedPrice, '.');
+		$price_integer = substr($formattedPrice, 0, $dotpos);
+		$price_fractional = substr($formattedPrice, $dotpos + 1);
+
+		$formattedPriceSU = sprintf('%1.02F', $signupFee * $vatMultiplier);
+		$dotposSU = strpos($formattedPriceSU, '.');
+		$price_integerSU = substr($formattedPriceSU, 0, $dotposSU);
+		$price_fractionalSU = substr($formattedPriceSU, $dotposSU + 1);
+
+		$formattedPriceD = sprintf('%1.02F', $discount);
+		$dotposD = strpos($formattedPriceD, '.');
+		$price_integerD = substr($formattedPriceD, 0, $dotposD);
+		$price_fractionalD = substr($formattedPriceD, $dotposD + 1);
+
+		$formattedPreDiscount = sprintf('%1.02F', $preDiscount);
+		$dotposPD = strpos($formattedPreDiscount, '.');
+		$price_integerPD = substr($formattedPreDiscount, 0, $dotposPD);
+		$price_fractionalPD = substr($formattedPreDiscount, $dotposPD + 1);
+
+		self::$pricingInformationCache[$levelKey] = (object)[
+			'vatRule'              => $vatRule,
+			'vatMultiplier'        => $vatMultiplier,
+			'levelPrice'           => $levelPrice,
+
+			'discount'             => $discount,
+			'discountFormatted'    => $formattedPriceD,
+			'discountInteger'      => $price_integerD,
+			'discountFractional'   => $price_fractionalD,
+
+			'prediscount'             => $preDiscount,
+			'prediscountFormatted'    => $formattedPreDiscount,
+			'prediscountInteger'      => $price_integerPD,
+			'prediscountFractional'   => $price_fractionalPD,
+
+			'formattedPrice'       => $formattedPrice,
+			'priceInteger'         => $price_integer,
+			'priceFractional'      => $price_fractional,
+			'formattedPriceSignup' => $formattedPriceSU,
+			'priceForFormatting'   => $priceForFormatting,
+
+			'signupFee'            => $signupFee,
+			'signupInteger'        => $price_integerSU,
+			'signupFractional'     => $price_fractionalSU,
+		];
+
+		return self::$pricingInformationCache[$levelKey];
+	}
+
+	/**
+	 * Format the price with the currency symbol
+	 *
+	 * @param   float  $rawPrice  The raw price
+	 *
+	 * @return  string
+	 */
+	public static function toCurrency($rawPrice)
+	{
+		$params = self::getPricingParameters();
+		$currencyPosition = $params->currencyPosition;
+
+		$convertedPriceInfo = Forex::convertToLocal($params->country, $rawPrice);
+
+		$result = sprintf('%0.2f', $convertedPriceInfo['value']);
+
+		if ($currencyPosition == 'before')
+		{
+			return $convertedPriceInfo['symbol'] . $result;
+		}
+
+		return $result . $convertedPriceInfo['symbol'];
+	}
+
+}
--- component/frontend/View/Levels/Html.php
+++ component/frontend/View/Levels/Html.php
@@ -10,6 +10,7 @@ namespace Akeeba\Subscriptions\Site\View\Levels;
 defined('_JEXEC') or die;
 
 use Akeeba\Subscriptions\Admin\Helper\Forex;
+use Akeeba\Subscriptions\Admin\Helper\Price;
 use Akeeba\Subscriptions\Site\Model\Levels;
 use Akeeba\Subscriptions\Site\Model\Subscriptions;
 use Akeeba\Subscriptions\Site\Model\TaxHelper;
@@ -116,73 +117,21 @@ class Html extends \FOF30\View\DataView\Html
 
 	public function applyViewConfiguration()
 	{
-// Cache subscription IDs of this user
-		$subIDs = array();
-		$user   = \JFactory::getUser();
+		// Transfer the parameters from the helper to the View
+		$params = Price::getPricingParameters();
 
-		if ($user->id)
-		{
-			/** @var Subscriptions $mysubs */
-			$mysubs = $this->container->factory->model('Subscriptions')->tmpInstance();
-			$mysubs
-				->user_id($user->id)
-				->paystate('C')
-				->get(true);
-
-			if (!empty($mysubs))
-			{
-				foreach ($mysubs as $sub)
-				{
-					$subIDs[] = $sub->akeebasubs_level_id;
-				}
-			}
-
-			$subIDs = array_unique($subIDs);
-		}
-
-		$this->subIDs = $subIDs;
-
-		// Should I show VAT?
-		$this->showVat = $this->container->params->get('showvat', 0);
-
-		// Cache tax parameters
-		/** @var \Akeeba\Subscriptions\Site\Model\TaxHelper $taxModel */
-		$this->taxModel  = $this->getContainer()->factory->model('TaxHelper')->tmpInstance();
-		$this->taxParams = $this->taxModel->getTaxDefiningParameters();
-
-		// Should I include sign-up one time fees?
-		$this->includeSignup = $this->container->params->get('includesignup', 2);
-
-		// Should I include discounts? (only valid if it's a logged in user)
-		$this->includeDiscount = false;
-
-		if (!$user->guest)
-		{
-			$this->includeDiscount = $this->container->params->get('includediscount', 0);
-		}
-
-		// Should I render zero prices as "FREE"?
-		$this->renderAsFree = $this->container->params->get('renderasfree', 0);
-
-		// Should I display prices in local currency based on country selection?
-		$this->showLocalPrices = $this->container->params->get('showlocalprices', 1);
-
-		// Update the rates and check the conversion rate for this display
-		if ($this->showLocalPrices)
-		{
-			$this->country = $this->taxParams['country'];
-			Forex::updateRates(false, $this->container);
-			$temp                = Forex::convertToLocal($this->taxParams['country'], 1.00, $this->container);
-			$this->exchangeRate  = $temp['rate'];
-			$this->localCurrency = $temp['currency'];
-			$this->localSymbol   = $temp['symbol'];
-		}
-
-		// Do not show foreign exchange conversions unless the exchange rate is different than unity
-		if (abs($this->exchangeRate - 1.00) <= 0.000001)
-		{
-			$this->showLocalPrices = false;
-		}
+		$this->subIDs          = Price::getSubIDs();
+		$this->showVat         = $params->showVat;
+		$this->taxModel        = $params->taxModel;
+		$this->taxParams       = $params->taxParams;
+		$this->includeSignup   = $params->includeSignup;
+		$this->includeDiscount = $params->includeDiscount;
+		$this->renderAsFree    = $params->renderAsFree;
+		$this->showLocalPrices = $params->showLocalPrices;
+		$this->country         = $params->country;
+		$this->exchangeRate    = $params->exchangeRate;
+		$this->localCurrency   = $params->localCurrency;
+		$this->localSymbol     = $params->localSymbol;
 	}
 
 	/**
@@ -204,146 +153,18 @@ class Html extends \FOF30\View\DataView\Html
 	 */
 	public function getLevelPriceInformation(Levels $level)
 	{
-		$levelKey = $level->getId() . '-' . $level->slug;
-
-		if (isset($this->pricingInformationCache[$levelKey]))
-		{
-			return $this->pricingInformationCache[$levelKey];
-		}
-
-		$signupFee = 0;
-		$discount = 0;
-		$levelPrice = $level->price;
-		$vatMultiplier = 1.0;
-		$vatRule = (object)[
-			'match'   => 0,    // How many parameters matched exactly
-			'fuzzy'   => 0,    // How many parameters matched fuzzily
-			'taxrate' => 0, // Tax rate in percentage points (e.g. 12.3 means 12.3% tax)
-			'id'      => 0, // The ID of the tax rule in effect
-		];
-
-		if (!in_array($level->akeebasubs_level_id, $this->subIDs) && ($this->includeSignup != 0))
-		{
-			$signupFee = (float)$level->signupfee;
-		}
-
-		if ($this->showVat)
-		{
-			$vatRule = $this->taxModel->getTaxRule(
-				$level->akeebasubs_level_id, $this->taxParams['country'], $this->taxParams['state'],
-				$this->taxParams['city'], $this->taxParams['vies']
-			);
-
-			$vatMultiplier = (100 + (float)$vatRule->taxrate) / 100;
-		}
-
-		$preDiscount = max($levelPrice, 0.0);
-
-		if ($this->includeDiscount)
-		{
-			/** @var \Akeeba\Subscriptions\Site\Model\Subscribe $subscribeModel */
-			$subscribeModel = $this->getContainer()->factory->model('Subscribe')->savestate(0);
-			$subscribeModel->setState('id', $level->akeebasubs_level_id);
-			$subscribeModel->setState('slug', $level->slug);
-			$subValidation = $subscribeModel->getValidation(true);
-			$discount = $subValidation->price->discount;
-			$levelPrice = $level->price - $discount;
-		}
-
-		if ($this->includeSignup == 1)
-		{
-			if (($levelPrice + $signupFee) < 0)
-			{
-				$levelPrice = -$signupFee;
-			}
-
-			$priceForFormatting = ($levelPrice + $signupFee) * $vatMultiplier;
-			$formattedPrice     = sprintf('%1.02F', $priceForFormatting);
-			$preDiscount        = max(($preDiscount + $signupFee) * $vatMultiplier, 0);
-			$levelPrice += $signupFee;
-		}
-		else
-		{
-			if ($levelPrice < 0)
-			{
-				$levelPrice = 0;
-			}
-
-			$priceForFormatting = ($levelPrice) * $vatMultiplier;
-			$formattedPrice = sprintf('%1.02F', $priceForFormatting);
-			$preDiscount = $preDiscount * $vatMultiplier;
-		}
-
-		$dotpos = strpos($formattedPrice, '.');
-		$price_integer = substr($formattedPrice, 0, $dotpos);
-		$price_fractional = substr($formattedPrice, $dotpos + 1);
-
-		$formattedPriceSU = sprintf('%1.02F', $signupFee * $vatMultiplier);
-		$dotposSU = strpos($formattedPriceSU, '.');
-		$price_integerSU = substr($formattedPriceSU, 0, $dotposSU);
-		$price_fractionalSU = substr($formattedPriceSU, $dotposSU + 1);
-
-		$formattedPriceD = sprintf('%1.02F', $discount);
-		$dotposD = strpos($formattedPriceD, '.');
-		$price_integerD = substr($formattedPriceD, 0, $dotposD);
-		$price_fractionalD = substr($formattedPriceD, $dotposD + 1);
-
-		$formattedPreDiscount = sprintf('%1.02F', $preDiscount);
-		$dotposPD = strpos($formattedPreDiscount, '.');
-		$price_integerPD = substr($formattedPreDiscount, 0, $dotposPD);
-		$price_fractionalPD = substr($formattedPreDiscount, $dotposPD + 1);
-
-		$this->pricingInformationCache[$levelKey] = (object)[
-			'vatRule'              => $vatRule,
-			'vatMultiplier'        => $vatMultiplier,
-			'levelPrice'           => $levelPrice,
-
-			'discount'             => $discount,
-			'discountFormatted'    => $formattedPriceD,
-			'discountInteger'      => $price_integerD,
-			'discountFractional'   => $price_fractionalD,
-
-			'prediscount'             => $preDiscount,
-			'prediscountFormatted'    => $formattedPreDiscount,
-			'prediscountInteger'      => $price_integerPD,
-			'prediscountFractional'   => $price_fractionalPD,
-
-			'formattedPrice'       => $formattedPrice,
-			'priceInteger'         => $price_integer,
-			'priceFractional'      => $price_fractional,
-			'formattedPriceSignup' => $formattedPriceSU,
-			'priceForFormatting'   => $priceForFormatting,
-
-			'signupFee'            => $signupFee,
-			'signupInteger'        => $price_integerSU,
-			'signupFractional'     => $price_fractionalSU,
-		];
-
-		return $this->pricingInformationCache[$levelKey];
+		return Price::getLevelPriceInformation($level);
 	}
 
+	/**
+	 * Format the price with the currency symbol
+	 *
+	 * @param   float  $rawPrice  The raw price
+	 *
+	 * @return  string
+	 */
 	public function toLocalCurrency($rawPrice)
 	{
-		static $currencyPosition = null;
-
-		if (is_null($currencyPosition))
-		{
-			$currencyPosition = $this->container->params->get('currencypos','before');
-		}
-
-		$convertedPriceInfo = Forex::convertToLocal($this->country, $rawPrice);
-
-		$result = sprintf('%0.2f', $convertedPriceInfo['value']);
-
-		if ($currencyPosition == 'before')
-		{
-			$result = $convertedPriceInfo['symbol'] . $result;
-		}
-		else
-		{
-			$result .= $convertedPriceInfo['symbol'];
-		}
-
-		return $result;
+		return Price::toCurrency($rawPrice);
 	}
 }
